0a3d809a9c94cfc38ebaeed914bdc0699245c43b
Administrator <Administrator@EC2AMAZ-QJ182TF>
2023-10-26 15:59:36 +0000

    Your commit message here

--- Day02AllInputs/MainWindow.xaml.cs
+++ Day02AllInputs/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +21,75 @@ namespace Day02AllInputs
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        public MainWindow() 
         {
-            InitializeComponent();
+            InitializeComponent();  
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string name = TbxName.Text;
+            if (name == "")
+            {
+                MessageBox.Show("Name must not be empty", "Input error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton radioButton = (RadioButton)sender;
+            string age = radioButton.Content.ToString();
+            if(age == "")
+            {
+                MessageBox.Show("Age must not be empty", "Input error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox comboBox = (ComboBox)sender;
+            string continent = ((ComboBoxItem)comboBox.SelectedItem).Content.ToString();
+            if (continent == "")
+            {
+                MessageBox.Show("Age must not be empty", "Input error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+        }
+
+        private void CheckBox_Click(object sender, RoutedEventArgs e)
+        {
+            CheckBox checkBox = (CheckBox)sender;
+            string pet = checkBox.Content.ToString();
+            if (pet == "")
+            {
+                MessageBox.Show("Pet must not be empty", "Input error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            //if (checkBox == chkCat || checkBox == chkDog)
+            //{
+            //    // If "Cat" or "Dog" is clicked, uncheck "Other."
+            //    chkOther.IsChecked = false;
+            //}
+            //else if (checkBox == chkOther)
+            //{
+            //    // If "Other" is clicked, uncheck "Cat" and "Dog."
+            //    chkCat.IsChecked = false;
+            //    chkDog.IsChecked = false;
+            //}
+        }
+
+        private void tempSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            int selectedAge = (int)e.NewValue;
+            string temp = selectedAge.ToString();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }

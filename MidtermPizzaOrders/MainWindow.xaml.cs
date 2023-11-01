b434926984ed9a60e23d2eb7ab57539c8e3d57d5
xiaoxing pan <panxx1988@gmail.com>
2023-11-01 18:54:21 +0000

    update

--- MidtermPizzaOrders/MainWindow.xaml.cs
+++ MidtermPizzaOrders/MainWindow.xaml.cs
@@ -91,5 +91,14 @@ namespace MidtermPizzaOrders
             LvOrders.ItemsSource = Globals.dbContextAuto.PizzaOrders.ToList();
         }
 
+        private void MenuItemeExit_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Are you sure you want to quit?", "Confirmation", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                Application.Current.Shutdown();
+            }
+        }
     }
 }

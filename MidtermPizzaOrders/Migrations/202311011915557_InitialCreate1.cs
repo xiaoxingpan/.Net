cfdd73730979122bcbca99084d49c7ea29ea08e8
xiaoxing pan <panxx1988@gmail.com>
2023-11-14 01:19:25 +0000

    update

---
+++ MidtermPizzaOrders/Migrations/202311011915557_InitialCreate1.cs
@@ -0,0 +1,18 @@
+﻿namespace MidtermPizzaOrders.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate1 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.PizzaOrders", "Extras", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.PizzaOrders", "Extras");
+        }
+    }
+}
